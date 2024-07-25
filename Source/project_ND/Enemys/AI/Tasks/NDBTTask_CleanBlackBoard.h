748aa23a4ef1e8362b2b08bd2f63c0dc0f417938
myeonghoon <mhoo999@naver.com>
2024-07-25 20:00:46 +0900

    AI_Chase_WIP
    - Behavior Tree 자연스럽게 이어지도록...

---
+++ Source/project_ND/Enemys/AI/Tasks/NDBTTask_CleanBlackBoard.h
@@ -0,0 +1,25 @@
+﻿// Copyright by project_ND
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "NDBTTask_CleanBlackBoard.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_ND_API UNDBTTask_CleanBlackBoard : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UNDBTTask_CleanBlackBoard();
+
+	UPROPERTY(EditAnywhere, Category="Node")
+	TArray<FName> NodeToDelete;
+
+private:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
