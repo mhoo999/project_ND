af4fbfb6c9086d451bd9f36b6253460cad26368d
유명훈 (YU MYEONG HOON) <144771457+mhoo999@users.noreply.github.com>
2024-08-23 15:04:32 +0900

    pakaging wip

--- Source/project_ND/project_ND.Build.cs
+++ Source/project_ND/project_ND.Build.cs
@@ -11,7 +11,7 @@ public class project_ND : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[]
 		{
 			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput",
-			"AIModule", "NavigationSystem", "UMG", "ChaosNiagara"
+			"AIModule", "NavigationSystem", "UMG"
 		});
 
 	}
